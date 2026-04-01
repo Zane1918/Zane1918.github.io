136f0aa5f5fc7bf7e5875b291a048c5f348b5e82
Ja <ja@Jas-MacBook-Pro.local>
2026-04-01 22:49:16 +0800

    feat(i18n): replace index and blog pages with locale redirects

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,9 @@
-import React from 'react'
-import Layout from '../components/layout/Layout'
-import Hero from '../components/sections/Hero'
-import About from '../components/sections/About'
-import Experience from '../components/sections/Experience'
-import Featured from '../components/sections/Featured'
-import Projects from '../components/sections/Projects'
-import Contact from '../components/sections/Contact'
-import RecentPosts from '../components/sections/RecentPosts'
-
-export const Head = () => (
-  <>
-    <title>Zeng Jia | Algorithm Engineer</title>
-    <meta name="description" content="Algorithm Engineer at Trip.com. I build intelligent systems at scale." />
-  </>
-)
+import { useEffect } from 'react'
+import { navigate } from 'gatsby'
 
 export default function IndexPage() {
-  return (
-    <Layout>
-      <Hero />
-      <About />
-      <Experience />
-      <Featured />
-      <Projects />
-      <Contact />
-      <RecentPosts />
-    </Layout>
-  )
+  useEffect(() => {
+    navigate('/en/', { replace: true })
+  }, [])
+  return null
 }
